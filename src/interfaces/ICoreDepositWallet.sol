50e290569f4b2a1aee1d57b6283e7bd9d5a7d3e1
epoon-circle <elim.poon@circle.com>
2025-08-13 14:02:13 -0700

    [UNTRACKED] Change solc ver 0.7.6 and add eccp dep (#3)
    This PR updates the project Solidity version from 0.8.20 to 0.7.6 to
    match CCTP. This will allow for reuse of library functions and
    interfaces.

--- src/interfaces/ICoreDepositWallet.sol
+++ src/interfaces/ICoreDepositWallet.sol
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-pragma solidity ^0.8.13;
+pragma solidity 0.7.6;
 
 interface ICoreDepositWallet {
     function deposit(uint256 amount) external;
