76a21f1b8331e8f6406f3f242d5ba0cc1b716300
Maxime Berenshteyn <maxime.berenshteyn@circle.com>
2025-08-14 15:09:03 -0700

    [CCTPE-1999] Implement deposit and withdraw functionality (#8)
    ## Summary
    
    Implements CoreDepositWallet `deposit`, `depositFor`, and `transfer`
    functions. This is a partial implementation of these features, as this
    is still pending 1) initializer configuration 2) tests 3) additional
    comments on the interface contract, but will merge for now to unblock
    admin role setup.
    
    #
    
    **Story:** https://circlepay.atlassian.net/browse/CCTPE-1999

--- src/CoreDepositWallet.sol
+++ src/CoreDepositWallet.sol
@@ -17,14 +17,65 @@
  */
 pragma solidity 0.7.6;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ICoreDepositWallet} from "./interfaces/ICoreDepositWallet.sol";
 
+/**
+ * @title CoreDepositWallet
+ * @notice Contract for managing token deposits and transfers between HyperEVM and HyperCore.
+ */
 contract CoreDepositWallet is ICoreDepositWallet {
-    function deposit(uint256 amount) external override 
-    {
-        revert("deposit: not implemented");
+    // ============ Events ============
+    /**
+     * @notice Emitted when tokens are transferred into the contract.
+     * @dev Required for HyperCore to correctly process deposits from HyperEVM.
+     * @param from The address initiating the transfer.
+     * @param to The address receiving the tokens.
+     * @param amount The amount of tokens being transferred.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    /**
+     * @notice Emitted when tokens are withdrawn from the contract.
+     * @param to The address receiving the withdrawn tokens.
+     * @param value The amount of tokens being withdrawn.
+     */
+    event Withdraw(address to, uint256 value);
+
+    // ============ State Variables ============
+    
+    // The contract of the HyperEVM token that can be deposited and withdrawn.
+    IERC20 public immutable TOKEN_CONTRACT;
+    
+    // The system address for the token spot asset on HyperCore.
+    address public immutable TOKEN_SYSTEM_ADDRESS;
+
+    // ============ Constructor ============
+    /**
+     * @param tokenContractAddress The address of the token contract.
+     * @param tokenSystemAddress The system address for the token on HyperCore.
+     */
+    constructor (address tokenContractAddress, address tokenSystemAddress) public {
+        TOKEN_CONTRACT = IERC20(tokenContractAddress);
+        TOKEN_SYSTEM_ADDRESS = tokenSystemAddress;
     }
 
+    // ============ External Functions  ============
+    /**
+     * @notice Deposits tokens to credit the corresponding address on HyperCore.
+     * @param amount The amount of tokens being deposited.
+     */
+    function deposit(uint256 amount) external override 
+    {
+        _deposit(msg.sender, msg.sender, amount);
+    }
+
+    /**
+    * @notice Deposits tokens from one address on HyperEVM to credit another address on HyperCore.
+    * @param sender The address sending the tokens on HyperEVM.
+    * @param recipient The address receiving the tokens on HyperCore.
+    * @param amount The amount of tokens being deposited.
+    */
     function depositFor(
         address sender,
         address recipient,
@@ -33,11 +84,38 @@ contract CoreDepositWallet is ICoreDepositWallet {
         external 
         override 
     {
-        revert("depositFor: not implemented");
+        require(recipient != address(0), "Cannot deposit to zero address");
+        require(recipient != TOKEN_SYSTEM_ADDRESS, "Cannot deposit to system address");
+        require(recipient != address(this), "Cannot deposit to wallet address");
+        _deposit(sender, recipient, amount);
     }
 
+    /**
+    * @notice Handles the token transfer from the wallet contract to the recipient.
+    * @dev This function can only be called by the token's system address. This ensures
+    *      this is only used to receive tokens after a spotSend has been executed on HyperCore.
+    * @param to The address receiving the tokens.
+    * @param amount The amount of tokens being transferred.
+    */
     function transfer(address to, uint256 amount) external override 
     {
-        revert("transfer: not implemented");
+        require(msg.sender == TOKEN_SYSTEM_ADDRESS, "Only system address can transfer");
+        require(to != TOKEN_SYSTEM_ADDRESS, "Cannot transfer to system address");
+        require(TOKEN_CONTRACT.transfer(to, amount), "Transfer operation failed");
+        
+        emit Withdraw(to, amount);
+    }
+
+    /** 
+     * @dev Handles the token transfer from the sender to the wallet contract.
+     * @param _sender The address initiating the deposit on HyperCore.
+     * @param _recipient The address receiving the tokens on HyperEVM.
+     * @param _amount The amount of tokens being deposited.
+     */
+    function _deposit(address _sender, address _recipient, uint256 _amount) internal {
+        require(_amount > 0, "Amount must be greater than zero");
+        require(TOKEN_CONTRACT.transferFrom(_sender, address(this), _amount), "Transfer operation failed");
+
+        emit Transfer(_recipient, TOKEN_SYSTEM_ADDRESS, _amount);
     }
 }
